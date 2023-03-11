9b2c6a71a5c939962a2e2f37ac7635c4575b466b
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-03-11 22:31:33 +0100

    index

---
+++ src/components/Quizbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function QuizBar(props) {
+    
+    return (
+        <div className='quizBar'>
+            <h1>Choose your study type</h1>
+            <ul className='nav nav-pills nav-fill'>
+                <li className='col-sm-3 text-center'>
+                    <div className='nav-card'>
+                        Random text
+                    </div>
+
+                </li>
+
+            </ul>
+
+        </div>
+    )
+}
